b5889e3dcfac4316e19ab5d60a57d2e50eb1027c
mywave76 <mywave.reloaded@yahoo.com>
2017-05-19 15:58:16 +0200

    Chapter 3: Templates

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404, HttpResponse
+from django.shortcuts import render
 import datetime
 
 def hello(request):
@@ -6,8 +7,7 @@ def hello(request):
 
 def current_datetime(request):
 	now = datetime.datetime.now()
-	html = '<html><h1>It is now {now}</h1></html>'.format(now=now)
-	return HttpResponse(html)
+	return render(request, 'current_datetime.html', {'current_date': now})
 
 def hours_ahead(request, offset):
 	try:
@@ -15,5 +15,4 @@ def hours_ahead(request, offset):
 	except ValueError:
 		raise Http404
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	html = '<html><h1>In {offset} hour(s), it will be {dt}.</h1></html>'.format(offset=offset, dt=dt)
-	return HttpResponse(html)
+	return render(request, 'hours_ahead.html', {'hour_offset': offset, 'next_time': dt})
